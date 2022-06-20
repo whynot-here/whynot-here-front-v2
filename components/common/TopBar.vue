cca96d3a8706a4472eef5c47cf33bb57c533b3f9
maker-dasu <winter7574@gmail.com>
2022-06-21 08:07:40 +0900

    feat: 로그인 모달 화면 제작
    Related to: #3

---
+++ components/common/TopBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div id="TopBar">
+    <div class="topbar">
+      <div class="logo">
+        <img src="@/assets/img/common/whynot-here-logo.png" alt="">
+      </div>
+      <div class="search"></div>
+      <div
+        class="login"
+        @click="loginPopupOpen = true"
+      >
+        로그인
+      </div>
+      <div class="profile">
+        <img src="@/assets/img/common/no-profile.png" alt="">
+      </div>
+    </div>
+    <LoginPopup
+      v-if="loginPopupOpen"
+      @closePopup="closeLoginPopup"
+    />
+  </div>
+</template>
+
+<script>
+import LoginPopup from '@/components/login/LoginPopup'
+
+export default {
+  name: 'TopBar',
+  components: {
+    LoginPopup
+  },
+  data () {
+    return {
+      loginPopupOpen: false
+    }
+  },
+  methods: {
+    closeLoginPopup () {
+      this.loginPopupOpen = false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#TopBar {
+  width: 1368px; height: 73px;
+  margin: 0 auto;
+  .topbar {
+    display: flex;
+    .logo {
+      padding-top: 16px;
+      img {
+        width: 139px; height: 33px;
+      }
+    }
+    .search {
+      flex-grow: 1;
+    }
+    .login {
+      padding-top: 24px; margin-right: 32px;
+      font-size: .94rem; font-weight: 700;
+      color: #737373;
+      cursor: pointer;
+    }
+    .profile {
+      padding-top: 16px;
+      img {
+        width: 32px;
+        height: 32px;
+      }
+    }
+  }
+}
+</style>
--- components/login/LoginPopup.vue
+++ components/login/LoginPopup.vue
@@ -1,21 +1,49 @@
 <template>
   <div id="LoginPopup" @click.self="$emit('closePopup')">
     <div class="popup-wrp">
-      <div class="popup-top">
-        <div class="close-btn" style="cursor: pointer;" @click="$emit('closePopup')">
-          X
+      <section class="popup-top">
+        <div
+          class="close-btn"
+          @click="$emit('closePopup')"
+        >
+          <img src="@/assets/img/login/close-btn.png" alt="">
         </div>
-        <h1>
-          Why Not Here 시작하기
-        </h1>
-      </div>
+      </section>
+      <section class="popup-middle">
+        <div class="top">
+          <div>사람이 모이는 공간</div>
+          <div>
+            <img src="@/assets/img/common/whynot-here-logo.png" alt="">
+          </div>
+        </div>
+        <div class="middle">
+          시작하기
+        </div>
+        <div class="social-login-btn">
+          <img src="@/assets/img/login/google-login-btn.png" alt="">
+        </div>
+        <div class="social-login-btn">
+          <img src="@/assets/img/login/kakao-login-btn.png" alt="">
+        </div>
+        <div class="social-login-btn">
+          <img src="@/assets/img/login/naver-login-btn.png" alt="">
+        </div>
+      </section>
+      <section class="popup-bottom">
+        로그인은 <strong>개인 정보 보호 정책</strong> 및 <strong>서비스 약관에 동의</strong>하는 것을
+        의미하며, 서비스 이용을 위해서 이메일과, 이름, 프로필 이미지를 수집합니다.
+      </section>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginPopup'
+  name: 'LoginPopup',
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
@@ -25,23 +53,73 @@ export default {
   position: fixed;
   width: 100vw; height: 100vh;
   top: 0; left: 0;
-  background: rgba(34,34,38,.6);
+  background: #D9D9D9;
   transition: opacity .2s;
   .popup-wrp {
-    max-width: 800px; height: 600px;
-    margin: calc((100vh - 600px) / 2) auto;
+    width: 422px; height: 496px;
+    margin: calc((100vh - 496px) / 2) auto;
 
-    background: #fff;
-    box-shadow: 0 0 20px 0 rgb(92 95 112 / 20%);
-    border-radius: 10px;
-    color: #18191b;
-    overflow: hidden;
-    text-align: center;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.04), 0px 8px 16px rgba(0, 0, 0, 0.08);
+    border-radius: 12px;
     .popup-top {
       .close-btn {
-        height: 60px; line-height: 60px;
-        padding-right: 20px;
+        padding-top: 24px; padding-right: 24px;
         text-align: right;
+        cursor: pointer;
+        img {
+          width: 32px; height: 32px;
+        }
+      }
+    }
+    .popup-middle {
+      width: 300px;
+      margin: 0 auto;
+      .top {
+        display: flex;
+        width: max-content; height: 28px;
+        margin: 0 auto;
+        div:first-child {
+          line-height: 29px;
+          font-size: .75rem; font-weight: 500;
+          color: #737373;
+        }
+        div:nth-child(2) {
+          margin-left: 6px;
+          img {
+            width: 120px; height: 28px;
+          }
+        }
+      }
+      .middle {
+        margin-top: 20px; padding-bottom: 50px;
+        text-align: center;
+        font-size: 1.38rem; font-weight: 700;
+        color: #181818;
+      }
+      .social-login-btn {
+        // height: 65px;
+        margin-bottom: 15px;
+        cursor: pointer;
+        img {
+          width: 300px; height: 50px;
+        }
+        img:hover {
+          box-shadow: 0px 0px 4px rgba(30, 30, 30, 0.04), 0px 4px 8px rgba(30, 30, 30, 0.04);
+          border-radius: 12px;
+        }
+      }
+    }
+    .popup-bottom {
+      width: 348px; line-height: 1.2rem;
+      margin: 0 auto; padding-top: 17px;
+      font-size: .75rem; font-weight: 400;
+      color: #7A7A7A;
+      text-align: center;
+      strong {
+        text-decoration: underline;
+        font-weight: 400;
+        cursor: pointer;
       }
     }
   }
