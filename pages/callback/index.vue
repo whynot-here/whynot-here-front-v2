84755aed5d62654873c30d85c368125a491afe13
maker-dasu <winter7574@gmail.com>
2022-06-21 21:25:21 +0900

    feat: 구글 로그인 기능 구현
    Related to: #3

---
+++ pages/callback/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <TopBar />
+  </div>
+</template>
+
+<script>
+import TopBar from '@/components/common/TopBar'
+
+export default {
+  name: 'CallBackPage',
+  components: {
+    TopBar
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    mounted () {
+      console.log('hihih')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
