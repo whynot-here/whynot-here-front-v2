65c62bcb3a8a532ef3ce21f6e858b2e968b89256
maker-dasu <winter7574@gmail.com>
2022-08-04 22:54:52 +0900

    fix: generate 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   target: 'static',
+  generate: {
+    fallback: true
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'whynot-here-front',
