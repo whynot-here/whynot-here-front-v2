5763c5b9d21287ae23b348e3def4cb5cbc151d5d
mean-ji <winter7574@gmail.com>
2022-10-28 13:00:00 +0900

    fix: head 태그 title 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'whynot-here-front',
+    title: 'whynot-here',
     htmlAttrs: {
       lang: 'en',
     },
