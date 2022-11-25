a2ef74ca878cd0b13870ada44ce1ca6e788701bc
osj3474 <handong3474@gmail.com>
2022-11-25 14:14:46 +0900

    feat: 마이페이지 모바일 UI 추가

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div id="MyPage">
-    <div v-if="!isMobile">
-      pcpc
-      <TopBarOnly class="top-bar-desktop" />
+  <div v-if="!isMobile" id="MyPage">
+    <TopBarOnly class="top-bar-desktop" />
     <section class="mypage-wrp">
       <div class="close">
         <img src="@/assets/img/common/close-page.png" alt="" @click="cmn_goMainPage">
@@ -22,9 +20,6 @@
           <div @click="editNickNameMode = true">
             {{ currentNickName }}
           </div>
-          <!-- <div class="edit-pencil">
-            <img src="@/assets/img/common/edit-pencil.png" alt="" @click="editNickNameMode = true">
-          </div> -->
         </div>
         <div v-else class="nickname selected">
           <div class="nickname-left">
@@ -42,49 +37,50 @@
         <input type="text" :value="$store.state.userInfo.detail.email" disabled>
       </div>
     </section>
-    </div>
-    <div v-else>
-      mobile
-      <section class="mypage-wrp">
-        <div class="close">
-          <img src="@/assets/img/common/close-page.png" alt="" @click="cmn_goMainPage">
+  </div>
+  <div v-else id="m-MyPage">
+    <section class="m-mypage-wrp">
+      <div class="m-mypage-header">
+        <div class="m-close">
+          <img class="m-back-btn" src="@/assets/img/common/left-arrow.png" alt="" @click="cmn_goMainPage">
         </div>
-        <div class="title">
+        <div class="m-title">
           마이페이지
         </div>
-        <div class="sub-title">
-          안녕하세요 <strong>{{currentNickName}}</strong> 님!
+      </div>
+
+      <div class="m-mypage-body">
+        <div>
+          <img class="m-profile-img" :src="$store.state.userInfo.detail.profileImg" alt="">
         </div>
-        <div class="img-nickname-grp">
-          <div class="img"><img :src="$store.state.userInfo.detail.profileImg" alt=""></div>
-          <div v-if="!editNickNameMode" class="nickname">
-            <div class="nickname-left">
-              닉네임
-            </div>
-            <div @click="editNickNameMode = true">
-              {{ currentNickName }}
-            </div>
-            <!-- <div class="edit-pencil">
-              <img src="@/assets/img/common/edit-pencil.png" alt="" @click="editNickNameMode = true">
-            </div> -->
+        <div class="m-sub-title">
+          안녕하세요 <strong class="m-sub-title-highlight">{{currentNickName}}</strong> 님!
+        </div>
+        <div v-if="!editNickNameMode" class="m-nickname">
+          <div class="m-nickname-left">
+            닉네임
           </div>
-          <div v-else class="nickname selected">
-            <div class="nickname-left">
-              닉네임
-            </div>
-            <div class="edit-input-wrp">
-              <input v-model="inputNickName" class="edit-input" type="text">
-            </div>
-            <div class="edit-button-wrp">
-              <div @click="editNickName()">저장</div>
-            </div>
+          <div class="m-nickname-content" @click="editNickNameMode = true">
+            {{ currentNickName }}
           </div>
         </div>
-        <div class="email">
+        <div v-else class="m-nickname m-selected">
+          <div class="m-nickname-left">
+            닉네임
+          </div>
+          <div class="m-nickname-content">
+            <input v-model="inputNickName" class="m-edit-input" type="text">
+          </div>
+          <div class="m-edit-button-wrp">
+            <div @click="editNickName()">저장</div>
+          </div>
+        </div>
+        
+        <div class="m-email">
           <input type="text" :value="$store.state.userInfo.detail.email" disabled>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -139,11 +135,6 @@ export default {
 @import '@/assets/scss/util.scss';
 
 #MyPage {
-  @include mobile {
-    .top-bar-desktop {
-      display: none;
-    }
-  }
   .mypage-wrp {
     width: 970px; height: 572px;
     margin: 0 auto; padding-top: 123px;
@@ -240,6 +231,114 @@ export default {
         border-radius: 12px;
         border: none;
         text-align: center;
+      }
+    }
+  }
+}
+
+#m-MyPage {
+  .m-mypage-wrp {
+    padding-top: 5vh;
+
+    .m-mypage-header {
+      height: 6vh;
+      display: grid;
+      grid-template:
+        "arrow title ." 1fr / 1fr 5fr 1fr;
+
+      .m-close {
+        width: 3vw;
+        padding-left: 5vw;
+        grid-area: arrow;
+        display: flex;
+        align-self: center;
+      }
+
+      .m-title {
+        color: #737373;
+        grid-area: title;
+        display: flex;
+        justify-self: center;
+        align-self: center;
+      }
+    }
+
+    .m-mypage-body {
+      background-color: #F3F3F3;
+      height: 100vh;
+      padding-top: 18vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .m-profile-img {
+        background-color: #FFFFFF;
+        width: 25vw;
+        margin-bottom: 3vh;
+        box-shadow: 0px 0px 4px rgba(0, 52, 138, 0.04), 0px 4px 8px rgba(0, 52, 138, 0.06);
+        border-radius: 18px;
+      }
+
+      .m-sub-title {
+        margin-bottom: 3vh;
+        .m-sub-title-highlight {
+          color: #3E82F1;
+        }
+      }
+
+      .m-nickname {
+        &.m-selected {
+          border: 1px solid #3E82F1;
+        }
+
+        display: grid;
+        width: 90%;
+        height: 5vh;
+        margin-bottom: 3vh;
+        background-color: #FFFFFF;
+        border-radius: 9999px;
+        box-shadow: 0px 0px 4px rgba(0, 52, 138, 0.04), 0px 8px 16px rgba(0, 52, 138, 0.08);
+        grid-template:
+          "left content right" 1fr / 1fr 4fr 1fr;
+        align-items: center;
+        padding-left: 20px;
+
+        .m-nickname-left {
+          grid-area: left;
+          color: #C8C8C8;
+          border-right: 1px solid #C8C8C8;
+        }
+
+        .m-nickname-content {
+          grid-area: content;
+          padding-left: 3vw;
+        }
+
+        .m-edit-button-wrp {
+          grid-area: right;
+          width: 12vw;
+          color: #3E82F1;
+          background: #F3F3F3;
+          border-radius: 16px;
+          text-align: center;
+        }
+      }
+
+      .m-email {
+        background-color: #EFEFEF;
+        border-radius: 9999px;
+        width: 90%;
+        height: 5vh;
+        display: flex;
+        align-items: center;
+
+        input {
+          width: 100%;
+          color: #A3A3A3;
+          text-align: center;
+          background-color: #EFEFEF;;
+          border: none;
+        }
       }
     }
   }
