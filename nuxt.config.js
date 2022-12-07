664a6aa39603eac78157ef8c7b3a61631eec85b2
mean-ji <winter7574@gmail.com>
2022-12-08 08:12:31 +0900

    fix: og:image 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: '@/assets/img/og/og-image.png'
+        content: '/og-image.png'
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
