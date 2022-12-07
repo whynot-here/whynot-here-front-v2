71461d9d563b938d48329056b0d83e4032f594c3
mean-ji <winter7574@gmail.com>
2022-12-08 08:36:02 +0900

    fix: og:image 확장자명 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: '/og-image.png'
+        content: '/og-image.jpeg'
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
