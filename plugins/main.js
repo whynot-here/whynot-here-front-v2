62bdf55727383b288dfcf144d2525e97e900dbe9
mean-ji <winter7574@gmail.com>
2022-12-08 14:12:12 +0900

    fix: bootstrap 에러 수정

--- plugins/main.js
+++ plugins/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 
-// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+// import { BootstrapVueIcons } from 'bootstrap-vue'
 
-// Vue.use(BootstrapVue)
-// Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.prototype.$bus = new Vue()
