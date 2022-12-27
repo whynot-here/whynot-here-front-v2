0d1f1699d33d10026619139d5d3104af92441f10
mean-ji <winter7574@gmail.com>
2022-12-27 21:50:44 +0900

    fix: 모집 마감 Dday 에러 수정

--- plugins/mixins/common.js
+++ plugins/mixins/common.js
@@ -181,14 +181,14 @@ const common = {
     cmn_getDday(endDate) {
         // 디데이 계산
         const today = new Date()
-        const end = new Date(endDate)
+        const end = new Date(endDate.replaceAll('-', '/'))
 
         const diff = (end.getTime() - today.getTime()) / (1000 * 3600 * 24)
         
         if (diff === 0) {
           return 'D-Day'
         } else {
-          return 'D' + (diff > 0 ? '-' + Math.ceil(diff) + '': '+' + Math.abs(Math.ceil(diff)) + '')
+          return 'D' + (diff > 0 ? '-' + Math.ceil(diff) : '+' + Math.abs(Math.ceil(diff)))
         }
     },
 
