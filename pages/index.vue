547412090917ccf39c15f812f9b069deffff15af
mean-ji <winter7574@gmail.com>
2023-03-04 20:17:09 +0900

    fix: netlify 배포 브랜치 변경

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <h3>why not here가 새롭게 오픈했습니다! 아래의 주소로 접속해주세요~</h3>
+    <a href="https://why-not-here.o-r.kr">whynothere 서비스</a>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
   mounted () {
-    if (this.$route.query.accessToken) {
-      this.cmn_getUserInfo(this.$route.query.accessToken)
-    }
-    this.$router.push('/gather/all')
+    // if (this.$route.query.accessToken) {
+    //   this.cmn_getUserInfo(this.$route.query.accessToken)
+    // }
+    // this.$router.push('/gather/all')
   },
   methods: {
   }
