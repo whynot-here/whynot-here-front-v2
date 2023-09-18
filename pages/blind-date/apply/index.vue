dc36704d05c73703306d8c8cfe3f57135b1d7ff9
mean-ji <winter7574@gmail.com>
2023-09-18 09:15:42 +0900

    style: 글 상세 댓글 버튼 css 수정

--- pages/blind-date/apply/index.vue
+++ pages/blind-date/apply/index.vue
@@ -415,66 +415,66 @@ export default {
             isShow: true,
             name: '',
             department: '',
-            studentId: ''
+            studentId: '',
           },
           {
             isShow: false,
             name: '',
             department: '',
-            studentId: ''
+            studentId: '',
           },
           {
             isShow: false,
             name: '',
             department: '',
-            studentId: ''
-          }
-        ]
+            studentId: '',
+          },
+        ],
       },
       dateStyle: [
         {
           id: 'CAFE',
           name: '카페 데이트',
-          url: require('@/assets/img/blind-date/cafe.png')
+          url: require('@/assets/img/blind-date/cafe.png'),
         },
         {
           id: 'HEALTH',
           name: '방탈출, 헬스장 데이트',
-          url: require('@/assets/img/blind-date/exercise.png')
+          url: require('@/assets/img/blind-date/exercise.png'),
         },
         {
           id: 'WALK',
           name: '산책, 걷기 데이트',
-          url: require('@/assets/img/blind-date/jogging.png')
-        }
+          url: require('@/assets/img/blind-date/jogging.png'),
+        },
       ],
       hobby: [
         {
           id: 'HOME',
           name: '나는 집이 좋은 집돌이, 집순이',
-          url: require('@/assets/img/blind-date/home.png')
+          url: require('@/assets/img/blind-date/home.png'),
         },
         {
           id: 'OUTSIDE',
           name: '주말 바깥 공기 필수!',
-          url: require('@/assets/img/blind-date/exercise.png')
-        }
+          url: require('@/assets/img/blind-date/exercise.png'),
+        },
       ],
       faith: [
         {
           id: 'NO_MATTER',
           name: '상관없어요',
-          url: require('@/assets/img/blind-date/heart.png')
+          url: require('@/assets/img/blind-date/heart.png'),
         },
         {
           id: 'CHRISTIAN',
           name: '기독교이신 분과만 매칭을 원해요',
-          url: require('@/assets/img/blind-date/christian.png')
-        }
+          url: require('@/assets/img/blind-date/christian.png'),
+        },
       ],
       isAddBtnActive: true,
       isNextActive: false,
-      isShow: false
+      isShow: false,
     }
   },
   watch: {},
@@ -502,8 +502,8 @@ export default {
         .get(`${process.env.apiUrl}/v2/blind-date/participation?season=1`, {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: this.$store.state.userInfo.token
-          }
+            Authorization: this.$store.state.userInfo.token,
+          },
         })
         .then((res) => {
           if (res.data) {
@@ -537,14 +537,13 @@ export default {
     },
     submit() {
       this.setSubmitParams()
-      console.log(this.applyParams)
       this.$axios
         .post(`${process.env.apiUrl}/v2/blind-date`, this.applyParams, {
           withCredentials: true,
           headers: {
             'Content-Type': 'application/json',
-            Authorization: this.$store.state.userInfo.token
-          }
+            Authorization: this.$store.state.userInfo.token,
+          },
         })
         .then((res) => {
           this.$router.push('/blind-date/proceeding')
@@ -583,8 +582,8 @@ export default {
       if (idx === 2) {
         this.isAddBtnActive = false
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
