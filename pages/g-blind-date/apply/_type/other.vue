59d748e6ad2ec37dafbceb5f3c5dbdf0c85c315f
handong3474 <handong3474@gmail.com>
2023-12-20 23:27:12 +0900

    fix: 선호 나이 클릭 시 상태 체크 누락 수정

--- pages/g-blind-date/apply/_type/other.vue
+++ pages/g-blind-date/apply/_type/other.vue
@@ -65,7 +65,7 @@
                     ? 'button-half selected'
                     : 'button-half'
                 "
-                @click="applyParams.favoriteAge = 'NO_MATTER'"
+                @click="[applyParams.favoriteAge = 'NO_MATTER', checkIsNextActive(1)]"
               >
                 상관없음
               </div>
@@ -75,7 +75,7 @@
                     ? 'button-half selected'
                     : 'button-half'
                 "
-                @click="applyParams.favoriteAge = 'SAME'"
+                @click="[applyParams.favoriteAge = 'SAME', checkIsNextActive(1)]"
               >
                 동갑
               </div>
@@ -87,7 +87,7 @@
                     ? 'button-half selected'
                     : 'button-half'
                 "
-                @click="applyParams.favoriteAge = 'UP'"
+                @click="[applyParams.favoriteAge = 'UP', checkIsNextActive(1)]"
               >
                 연상
               </div>
@@ -97,7 +97,7 @@
                     ? 'button-half selected'
                     : 'button-half'
                 "
-                @click="applyParams.favoriteAge = 'DOWN'"
+                @click="[applyParams.favoriteAge = 'DOWN', checkIsNextActive(1)]"
               >
                 연하
               </div>
